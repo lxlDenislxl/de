0026b9541d47d698e07cba9ac4ae610b58b48271
Гость <Guest@iMac-Mac005-3.local>
2022-05-05 14:47:55 +0900

    AS

---
+++ tablez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tablez
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    }
+
